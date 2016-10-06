dd9fb2d6ca9322d8e9282cd2629e43798a477ba3
Youri Westerman <tetracon@gmail.com>
2016-10-06 14:59:15 +0700

    Nested ArtistBrowseListViewOnItemClickListener in ArtistBrowseFragment

--- app/src/main/java/com/example/audioplayer/BrowseActivity.java
+++ app/src/main/java/com/example/audioplayer/BrowseActivity.java
@@ -76,7 +76,9 @@ public class BrowseActivity extends AppCompatActivity implements
                 mSortedAscending
         );
         fragment.setListAdapter(adapter);
-        fragment.setOnItemClickListener(new ArtistBrowseListViewOnItemClickListener(this));
+        fragment.setOnItemClickListener(
+                new ArtistBrowseFragment.ArtistBrowseListViewOnItemClickListener(this)
+        );
 
         return fragment;
     }
@@ -565,6 +567,47 @@ public class BrowseActivity extends AppCompatActivity implements
                 }
 
                 return false;
+            }
+        }
+
+        static final class ArtistBrowseListViewOnItemClickListener implements AdapterView.OnItemClickListener {
+            private BrowseActivity mActivity;
+
+            ArtistBrowseListViewOnItemClickListener(BrowseActivity activity) {
+                mActivity = activity;
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("4c0n", "onItemClick " + view.toString());
+
+                // TODO: start new activity
+
+                Bundle arguments = new Bundle();
+                arguments.putString(ArtistDetailsFragment.ARGUMENT_ARTIST_ID, "" + id);
+
+                ArtistDetailsExpandableListAdapter adapter = new ArtistDetailsExpandableListAdapter(
+                        mActivity.getLayoutInflater(),
+                        mActivity.getResources(),
+                        mActivity.getContentResolver()
+                );
+
+                ArtistDetailsFragment fragment = new ArtistDetailsFragment();
+                fragment.setArguments(arguments);
+                fragment.setAdapter(adapter);
+
+                Spinner browseTypeSpinner = (Spinner) mActivity.findViewById(R.id.browse_type_spinner);
+                browseTypeSpinner.setVisibility(View.GONE);
+
+                TextView artistNameTextView = (TextView) view.findViewById(R.id.browse_list_top_text);
+                TextView menuTextView = (TextView) mActivity.findViewById(R.id.menu_text);
+                menuTextView.setText(artistNameTextView.getText());
+                menuTextView.setVisibility(View.VISIBLE);
+
+                mActivity.getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.media_fragment_container, fragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         }
     }
