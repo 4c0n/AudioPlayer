48bf0406b373de3786af37a75644caaaf35927a5
Youri Westerman <tetracon@gmail.com>
2016-10-06 15:52:49 +0700

    Removed broken code

--- app/src/main/java/com/example/audioplayer/BrowseActivity.java
+++ app/src/main/java/com/example/audioplayer/BrowseActivity.java
@@ -594,33 +594,6 @@ public class BrowseActivity extends AppCompatActivity implements
                 Log.d("4c0n", "onItemClick " + view.toString());
 
                 // TODO: start new activity
-
-                Bundle arguments = new Bundle();
-                arguments.putString(ArtistDetailsFragment.ARGUMENT_ARTIST_ID, "" + id);
-
-                ArtistDetailsExpandableListAdapter adapter = new ArtistDetailsExpandableListAdapter(
-                        mActivity.getLayoutInflater(),
-                        mActivity.getResources(),
-                        mActivity.getContentResolver()
-                );
-
-                ArtistDetailsFragment fragment = new ArtistDetailsFragment();
-                fragment.setArguments(arguments);
-                fragment.setAdapter(adapter);
-
-                Spinner browseTypeSpinner = (Spinner) mActivity.findViewById(R.id.browse_type_spinner);
-                browseTypeSpinner.setVisibility(View.GONE);
-
-                TextView artistNameTextView = (TextView) view.findViewById(R.id.browse_list_top_text);
-                TextView menuTextView = (TextView) mActivity.findViewById(R.id.menu_text);
-                menuTextView.setText(artistNameTextView.getText());
-                menuTextView.setVisibility(View.VISIBLE);
-
-                mActivity.getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.media_fragment_container, fragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
         }
     }
 
