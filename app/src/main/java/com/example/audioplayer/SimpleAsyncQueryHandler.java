8f2aebf7e39af7a9fc3ead21b14f4d1cceb8538b
Youri Westerman <tetracon@gmail.com>
2016-10-11 17:41:35 +0700

    Added interface and callback facilities for async read queries

--- app/src/main/java/com/example/audioplayer/SimpleAsyncQueryHandler.java
+++ app/src/main/java/com/example/audioplayer/SimpleAsyncQueryHandler.java
@@ -2,24 +2,36 @@ package com.example.audioplayer;
 
 import android.content.AsyncQueryHandler;
 import android.content.ContentResolver;
+import android.database.Cursor;
 
 import java.util.ArrayList;
 
 
 class SimpleAsyncQueryHandler extends AsyncQueryHandler {
     private ArrayList<OnDeleteCompleteListener> mOnDeleteCompleteListeners;
+    private ArrayList<OnQueryCompleteListener> mOnQueryCompleteListeners;
 
     SimpleAsyncQueryHandler(ContentResolver cr) {
         super(cr);
         mOnDeleteCompleteListeners = new ArrayList<>(1);
+        mOnQueryCompleteListeners = new ArrayList<>(1);
     }
 
     @Override
     protected void onDeleteComplete(int token, Object cookie, int result) {
         super.onDeleteComplete(token, cookie, result);
 
-        for (OnDeleteCompleteListener onDeleteCompleteListener: mOnDeleteCompleteListeners) {
+        for (OnDeleteCompleteListener onDeleteCompleteListener : mOnDeleteCompleteListeners) {
             onDeleteCompleteListener.onDeleteComplete(token, cookie, result);
+        }
+    }
+
+    @Override
+    protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+        super.onQueryComplete(token, cookie, cursor);
+
+        for (OnQueryCompleteListener onQueryCompleteListener : mOnQueryCompleteListeners) {
+            onQueryCompleteListener.onQueryComplete(token, cookie, cursor);
         }
     }
 
@@ -37,7 +49,25 @@ class SimpleAsyncQueryHandler extends AsyncQueryHandler {
         }
     }
 
+    void registerOnQueryCompleteListener(OnQueryCompleteListener listener) {
+        if (!mOnQueryCompleteListeners.contains(listener)) {
+            mOnQueryCompleteListeners.add(listener);
+        }
+    }
+
+    void unregisterOnQueryCompleteListener(OnQueryCompleteListener listener) {
+        if (mOnQueryCompleteListeners.contains(listener)) {
+            mOnQueryCompleteListeners.remove(listener);
+        } else {
+            throw new RuntimeException("Trying to unregister a listener that is not registered...");
+        }
+    }
+
     interface OnDeleteCompleteListener {
         void onDeleteComplete(int token, Object cookie, int result);
+    }
+
+    interface OnQueryCompleteListener {
+        void onQueryComplete(int token, Object cookie, Cursor cursor);
     }
 }
